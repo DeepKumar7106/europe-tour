64f6d0b9052789830e8008c9638e521d25a8e15e
DeepKumar7106 <konata7106@gmail.com>
2026-02-04 17:48:02 +0530

    js bugs fixed

--- src/pages/RegionMain.jsx
+++ src/pages/RegionMain.jsx
@@ -4,37 +4,61 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { useEffect, useRef, useState } from 'react'
 
 export default function RegionMain() {
-    const [countryIndex , setCountryIndex] = useState(0)
+    const [countryIndex , setCountryIndex] = useState({
+        "index":0,
+        "userClick":false,
+    })
     const {regionId} = useParams()
     const navigate = useNavigate()
     const regionCard = useRef(null)
+    const regionListRef =  useRef()
+
     const currentRegion = regions.find(item => item.id === regionId)
     const countryList = countries.filter(country => country.region === regionId)
 
-    const currentCountry = countryList[countryIndex]
+    const currentCountry = countryList[countryIndex.index]
 
-    const updateCountryIndex = index => {setCountryIndex(index)}
-
+    const updateCountryIndex = index => {  
+        setCountryIndex(
+            {
+                ...countryIndex,
+                "index":index,
+                "userClick":true,
+            }
+        )
+    }
     useEffect(() => {
-        const listWrapper = document.getElementById('region-list-section')
+        const listWrapper = regionListRef.current
+        // const listWrapper = document.getElementById('region-list-section')
         listWrapper.style.height = `${countryList.length * 120}vh`
         const listWrapperHeight = Math.round(listWrapper.offsetHeight * 0.8)
-        console.log(listWrapperHeight)
         const lisStartPoint = listWrapper.getBoundingClientRect().top + window.scrollY
-        const listEndPoint = listWrapperHeight + lisStartPoint
-
         const ranges = countryList.map((c, index) => {
-            const start = Math.floor((index * 100) / countryList.length);
+            const startPer = Math.floor((index * 100) / countryList.length);
+            const start = index / countryList.length
             const end = Math.floor(((index + 1) * 100) / countryList.length);
-            return { start: index === 0 ? 0 : start + 1, end };
+            return { startPer: index === 0 ? 0 : startPer + 1, end , start};
         } )
-
+    
+        if(countryIndex.userClick) {
+            window.scrollTo({
+                top: ((ranges[countryIndex.index].start + 0.05) * listWrapperHeight) + lisStartPoint,
+                behavior: 'smooth'
+            })
+            setCountryIndex({
+                ...countryIndex,
+                "userClick":false,
+            })
+        }
         const handleScroll = () => {
             const currentScrollPoint = Math.floor((window.scrollY - lisStartPoint) / (listWrapperHeight / 100))
             
             ranges.forEach((range, index) => {
-                if(currentScrollPoint > range.start && currentScrollPoint < range.end)
-                    setCountryIndex(index)
+                if(currentScrollPoint > range.startPer && currentScrollPoint < range.end && !countryIndex.userClick)
+                    setCountryIndex({
+                        ...countryIndex,
+                        "index":index
+                    })
             })
         }
 
@@ -52,13 +76,13 @@ export default function RegionMain() {
                     <p id="heroPara">{currentRegion.desc}</p>
                 </article>
             </section>
-            <section id="region-list-section">
+            <section id="region-list-section" ref={regionListRef}>
                 <div className="region-country-card" key={currentCountry.id} id={currentCountry.id} ref={regionCard}>
                     <img className="region-country-card-bg-img" src={`/country-backdrop-webp/${currentCountry.name}.webp`} alt="" />
                     <div className="region-country-card-detail">
                         {countryList.map((c, index) => (
                             <button 
-                                className= {countryIndex === index ?"region-country-card-detail-card-index-current":"region-country-card-detail-card-index"} 
+                                className= {countryIndex.index === index ?"region-country-card-detail-card-index-current":"region-country-card-detail-card-index"} 
                                 id={index} 
                                 key={c.id}
                                 onClick={()=>{updateCountryIndex(index)}}
